e9f96d5e997a17b418dfa5a42d11e18c8a619fcc
Holase <zhongyan.zhang@ucdconnect.ie>
2020-03-31 23:44:06 +0100

    fix 3 bugs in user register module

--- app/routes.py
+++ app/routes.py
@@ -52,7 +52,6 @@ def logout():
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -61,4 +60,4 @@ def register():
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
-    render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title='Register', form=form)
