48ed85eb1873fbd187c7a1313b83868de77b6d61
Holase <zhongyan.zhang@ucdconnect.ie>
2020-04-02 17:36:33 +0100

    chapter07 add errorpages and log module

--- config.py
+++ config.py
@@ -9,3 +9,12 @@ class Config:
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
+    # email setting
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['zhangzhongyan2016@gmail.com']
+
