778803a87a34aca53151e70e00199c94c4dda220
Holase <zhongyan.zhang@ucdconnect.ie>
2020-04-03 13:34:48 +0100

    chapter08 add follower to databases;

--- app/models.py
+++ app/models.py
@@ -15,6 +15,12 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
     
@@ -26,6 +32,18 @@ class User(UserMixin, db.Model):
         gravatar_url = 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
         return gravatar_url
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+    
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
     
 
     def __repr__(self):
@@ -40,6 +58,12 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
 
 
 @login.user_loader
