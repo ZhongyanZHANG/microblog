0decc2a387b1a40f3769fd4bdd3e0adab9fd8f11
Holase <zhongyan.zhang@ucdconnect.ie>
2020-04-03 15:55:48 +0100

    chapter08 add follower and followed module

--- app/models.py
+++ app/models.py
@@ -6,6 +6,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db, login
 
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -45,6 +52,13 @@ class User(UserMixin, db.Model):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
     
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+    
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -59,11 +73,6 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-
-followers = db.Table('followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-    )
 
 
 @login.user_loader
